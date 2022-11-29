89cc48cacb5252e08dd2fab4625d1034f531be5d
NIMIDEV <99932058+NIMICODER@users.noreply.github.com>
2022-11-29 08:28:23 +0100

    Add project files.

---
+++ ATM Console App1/English Selection/App/Igala.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_Console_App1.App
+{
+    internal class Igala
+    {
+    }
+}
